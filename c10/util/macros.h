9a49a60ee13dc7c4b18edb11a9d7d26f248bf691
sunjiabin17 <sunjiabin01@outlook.com>
2024-09-19 17:12:08 +0800

    rename macros

--- c10/util/macros.h
+++ c10/util/macros.h
@@ -5,8 +5,9 @@
 #define LIKELY(expr) (__builtin_expect(static_cast<bool>(expr), 1))
 #define UNLIKELY(expr) (__builtin_expect(static_cast<bool>(expr), 0))
 
-#define C10_EXPORT __attribute__((visibility("default")))
-#define C10_IMPORT __attribute__((visibility("default")))
+#define C10_VISIBILE __attribute__((visibility("default")))
 #define C10_HIDDEN __attribute__((visibility("hidden")))
 
-#define C10_API C10_EXPORT
+#define C10_API C10_VISIBILE
+
+#define TORCH_API C10_VISIBILE
